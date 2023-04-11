a80560fe637841cecf6de4dde4b7afebdb10abd0
GSY <gle722@yandex.ru>
2023-04-11 08:18:44 +0300

    Решил задачу 4

---
+++ Lesson2/Task_4.py
@@ -0,0 +1,24 @@
+# Задача 4. Задайте список из N элементов, заполненных
+# числами из промежутка [-N, N]. Сдвиньте все элементы
+# списка на 2 позиции вправо.
+
+# 3 -> [2, 3, -3, -2, -1, 0, 1]
+
+number = int(
+            input("Введите число: "))
+
+numbers = list()
+if number < 0:
+    number = -number
+count = -number
+
+while count != number + 1:
+    numbers.append(count)
+    count += 1
+print(numbers)
+
+new_numbers = list()
+for i in range(0, len(numbers)):       
+    new_numbers.append(numbers[i - 2])
+
+print(new_numbers) 
