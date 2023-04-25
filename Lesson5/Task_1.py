1858ba615f53098f0c290ee0a83cde8ab90e268e
GSY <gle722@yandex.ru>
2023-04-25 09:33:10 +0300

    Решил задачу 1

---
+++ Lesson5/Task_1.py
@@ -0,0 +1,15 @@
+# Задача 1. Задайте список случайных чисел от 1 до 10,
+# выведите все элементы больше 5. Используйте для решения
+# лямбда-функцию.
+
+import random
+
+size = int(
+            input("Введите размер списка: "))
+numbers = [random.randint(1, 10) for i in range(size + 1)]
+print(numbers)
+
+numbers = list(
+                filter(
+                        lambda x: x > 5, numbers))
+print(numbers)
