520bf4870cea130b2fe361de4b1fbb9cb2c1029c
GSY <gle722@yandex.ru>
2023-05-26 22:20:50 +0300

    Решил задачу 2

--- Lesson8/Task_1.py
+++ Lesson8/Task_1.py
@@ -1,9 +1,8 @@
 # Задача 1. Напишите бота для техподдержки. Бот должен записывать обращения пользователей в файл.
 
 import telebot
-import random
 
-bot = telebot.TeleBot("6098227235:AAGK3FZwifIBtN02O3h9r9yV-rmVY-UYxK0")
+bot = telebot.TeleBot("")
 
 @bot.message_handler(commands=['обращение'])
 def request_handling(message):
@@ -12,7 +11,7 @@ def request_handling(message):
     @bot.message_handler(content_types=['text'])
     def requests(message):
         data = open('requests_log.txt', mode='a', encoding='utf-8')
-        data.writelines(f'{message.from_user.first_name} {message.from_user.last_name}: {message.text}\n')
+        data.writelines(f'{message.from_user.id} - {message.from_user.first_name} {message.from_user.last_name}: {message.text}\n')
         data.close()
 
 
